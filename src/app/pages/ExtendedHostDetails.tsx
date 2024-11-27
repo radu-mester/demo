2d006073715a2625fc3f1881b6fadb37a3fbf0aa
Radu <radumester@gmail.com>
2024-11-27 15:01:14 +0100

    v5

--- src/app/pages/ExtendedHostDetails.tsx
+++ src/app/pages/ExtendedHostDetails.tsx
@@ -1,105 +1,42 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import { Flex } from "@dynatrace/strato-components/layouts";
 import { TitleBar } from "@dynatrace/strato-components-preview/layouts";
-import {
-  TimeseriesChart,
-  ChartSeriesAction,
-} from "@dynatrace/strato-components-preview/charts";
+import { TimeseriesChart } from "@dynatrace/strato-components-preview/charts";
 import type { Timeseries } from "@dynatrace/strato-components-preview/charts";
-import { SimpleTable } from "@dynatrace/strato-components-preview/tables";
-import { MagnifyingGlassIcon } from "@dynatrace/strato-icons";
+import {
+  ToggleButtonGroup,
+  ToggleButtonGroupItem,
+} from "@dynatrace/strato-components-preview/buttons";
 import { mockTimeseriesDiskUsage } from "@dynatrace/strato-components-preview/mock-data";
-import { format } from "@dynatrace-sdk/units";
 
 export const ExtendedHostDetails = () => {
-  interface ExtendedTimeseries extends Timeseries {
-    region: string;
-    link: string;
-    host: string;
-    disk: string;
-    object: { number: number; text: string; isForTesting: boolean };
-  }
-
-  const customData = {
-    region: "us-east-1",
-    link: "https://www.dynatrace.com/",
-    object: {
-      text: "this is a complex object",
-      number: 1,
-      isForTesting: true,
-    },
-  };
-
-  const timeseriesWithCustomData: Timeseries[] = mockTimeseriesDiskUsage.map(
-    (series, index) => ({
-      ...series,
-      ...customData,
-      host: series.name[0],
-      disk: series.name[1],
-      object: {
-        ...customData.object,
-        number: index + 1,
-      },
-    })
+  const [chartVariant, setChartVariant] = useState<"default" | "area" | "bar">(
+    "default"
   );
-
-  const [selectedSeries, setSelectedSeries] = useState<ExtendedTimeseries>();
-
-  const columns = [
-    { id: "key", header: "Key", accessor: "key" },
-    { id: "value", header: "Value", accessor: "value" },
-  ];
-
-  const data = useMemo(() => {
-    if (selectedSeries) {
-      const { region, host, disk, link, object } = selectedSeries;
-      const { start, end, value } = selectedSeries.datapoints[0];
-      return [
-        { key: "Start", value: start.toLocaleString() },
-        { key: "End", value: end?.toLocaleString() },
-        { key: "Region", value: region },
-        { key: "Host", value: host },
-        { key: "Disk", value: disk },
-        { key: "Link", value: link },
-        { key: "Object", value: JSON.stringify(object) },
-        { key: "Value", value: format(value) },
-      ];
-    }
-    return [];
-  }, [selectedSeries]);
 
   return (
     <Flex width="100%" flexDirection="column" justifyContent="center" gap={16}>
       <TitleBar>
         <TitleBar.Title>Host Timeseries Details</TitleBar.Title>
       </TitleBar>
+
+      {/* Toggle Button Group for Chart Variant */}
+      <ToggleButtonGroup
+        value={chartVariant}
+        onChange={(value) =>
+          setChartVariant(value as "default" | "area" | "bar")
+        }
+      >
+        <ToggleButtonGroupItem value="default">Default</ToggleButtonGroupItem>
+        <ToggleButtonGroupItem value="area">Area</ToggleButtonGroupItem>
+        <ToggleButtonGroupItem value="bar">Bar</ToggleButtonGroupItem>
+      </ToggleButtonGroup>
+
+      {/* Dynamic TimeseriesChart */}
       <TimeseriesChart
-        data={timeseriesWithCustomData}
-        seriesActions={(seriesAction) => {
-          const action = seriesAction as ExtendedTimeseries;
-          return (
-            <ChartSeriesAction>
-              <ChartSeriesAction.Item
-                key={action.host}
-                onSelect={() => setSelectedSeries(action)}
-              >
-                <ChartSeriesAction.ItemIcon>
-                  <MagnifyingGlassIcon />
-                </ChartSeriesAction.ItemIcon>
-                Inspect data point
-              </ChartSeriesAction.Item>
-            </ChartSeriesAction>
-          );
-        }}
+        data={mockTimeseriesDiskUsage}
+        variant={chartVariant !== "default" ? chartVariant : undefined}
       />
-      {selectedSeries && (
-        <>
-          <h4>
-            Selected series: {selectedSeries.host} • {selectedSeries.disk}
-          </h4>
-          <SimpleTable columns={columns} data={data} />
-        </>
-      )}
     </Flex>
   );
 };
